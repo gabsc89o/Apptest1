36752977b2973ebd2bc6b4ddd493a0aa6a6a4131
gabsc89o <user138066@EU249.local>
2018-04-17 16:15:58 +0100

    change3
    Change3

--- Apptest1/AppDelegate.m
+++ Apptest1/AppDelegate.m
@@ -1,7 +1,7 @@
 //
 //  AppDelegate.m
 //  Apptest1
-// 45
+// 454545
 //  Created by user138066 on 4/17/18.
 //  Copyright © 2018 user138066. All rights reserved.
 //
@@ -17,33 +17,40 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    NSLog(@"Application Did Finish Launch");
     return YES;
 }
 
 
 - (void)applicationWillResignActive:(UIApplication *)application {
+    NSLog(@"Application Will resign");
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
     // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
 }
 
 
 - (void)applicationDidEnterBackground:(UIApplication *)application {
+    NSLog(@"Application did enter background");
     // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
     // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
 }
 
 
 - (void)applicationWillEnterForeground:(UIApplication *)application {
+    NSLog(@"Application Will enter foreground");
+    
     // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
 }
 
 
 - (void)applicationDidBecomeActive:(UIApplication *)application {
+    NSLog(@"Application Did become active");
     // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
 }
 
 
 - (void)applicationWillTerminate:(UIApplication *)application {
+    NSLog(@"Application Will terminate");
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
 }
 
--- Apptest1/ViewController.m
+++ Apptest1/ViewController.m
@@ -16,10 +16,33 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    NSLog(@"viewDidLoad");
     // Do any additional setup after loading the view, typically from a nib.
 }
 
+- (void)viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:<#animated#>]];
+    NSLog(@"viewWillAppear");
+    // Do any additional setup after loading the view, typically from a nib.
+}
 
+- (void)viewDidAppear:(BOOL)animated {
+    [super viewDidAppear:<#animated#>];
+    NSLog(@"viewDidAppear");
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)viewWillDisappear:(BOOL)animated {
+    [super viewWillDisappear:<#animated#>];
+    NSLog(@"viewWillDisappear");
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)viewDidDisappear:(BOOL)animated {
+    [super viewDidDisappear:animated];
+    NSLog(@"viewDidDisappear");
+    // Do any additional setup after loading the view, typically from a nib.
+}
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
